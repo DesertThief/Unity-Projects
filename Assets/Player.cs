31902b5cacc6d48a246eede432743a85782c45ed
DesertThief <37451229+DesertThief@users.noreply.github.com>
2018-05-13 20:46:22 +0300

    Add files via upload

---
+++ Assets/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Vertical"))
+		{
+
+			rb.bodyType = RigidbodyType2D.Dynamic;
+		} 
+	}
+}
